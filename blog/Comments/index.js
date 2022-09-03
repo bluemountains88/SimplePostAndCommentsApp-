b661e0557cd1499179b135eac42eaf8243006b44
bluemountains88 <m.dariouk@gmail.com>
2022-09-03 20:12:41 -0300

    Update comment status

--- blog/Comments/index.js
+++ blog/Comments/index.js
@@ -34,6 +34,30 @@ app.post('/posts/:id/comments',(req,res)=> {
 
     app.post('/events', (req,res) => {
         console.log('Event received', req.body.type);
+        
+        const { type, data } = req.body;
+
+        if (type === 'CommentModerated') {
+            const { postId, id, status, content } = data;
+            const comments = commentsByPostId[postId];
+            const comment = comments.find(comment => {
+                return comment.id = id;
+            });
+
+            comment.status = status;
+
+            axios.post('http://localhost:4005', {
+                type: 'CommentUpdated',
+                data: {
+                    id,
+                    status,
+                    postId,
+                    content                    
+                }
+            }).catch((err) => {
+                console.log(err.message);
+            })
+        }
         res.send({});
     })
 
