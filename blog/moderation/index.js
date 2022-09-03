ad8a0eb9deac300c4608c8d9be87349e0ca35971
bluemountains88 <m.dariouk@gmail.com>
2022-09-03 19:56:10 -0300

    implement moderation service

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -6,8 +6,25 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req,res) => {
+    const { type, data } = req.body;
 
-})
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') // random word to moderate -> orange
+            ? 'rejected'
+            : 'approved'
+        axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        }).catch((err) => {
+            console.log(err.message);
+        }) 
+    }
+});
 
 app.listen(4003, () => {
     console.log('Listening on port 4003');
